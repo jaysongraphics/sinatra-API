d3f371ae8b2d448d988391a81222d02052aed7dc
ShevDenny <sdenny904@gmail.com>
2021-07-14 20:44:55 -0400

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,38 +1,48 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::CrossOrigin
-
   configure do
     enable :cross_origin
     set :allow_origin, "*" 
     set :allow_methods, [:get, :post, :patch, :delete, :options] # allows these HTTP verbs
     set :expose_headers, ['Content-Type']
   end
-
   options "*" do
     response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
     200
   end
-
   # method "URL" 
     get '/'  do
       "Welcome to Artsy"
     end
-
     get '/painting'  do
       Painting.all.to_json
     end
-
     get '/buyer'  do
       Buyer.all.to_json
     end
-
+    post '/login'  do
+       params.to_json
+       username = params[:Username]
+       email = params[:Email]
+       Buyer.find_by(name: username, email: email).to_json
+    end
+    post '/signup'  do
+      params.to_json
+      username = params[:Username]
+      email = params[:Email]
+      Buyer.create(name: username, email: email).to_json
+   end
     get '/gallery'  do
       Gallery.all.to_json
     end
-
-    get '/reviews'  do
+    get '/review'  do
       Review.all.to_json
     end
-
+    post '/review'  do
+      Review.collect{|rev| rev.id == params[:id]}.to_json
+    end
+    delete '/buyer/:id' do
+      Buyer.find_by(id: params[:id]).destroy
+    end
 end
